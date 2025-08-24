275c26e0a4d4731b6a2b23ee32fd5b7fa14f1b65
SAM1044 <32495182+SAM1044@users.noreply.github.com>
2025-08-24 16:34:45 -0300

    Update apicheck-claim.js

--- apicheck-claim.js
+++ apicheck-claim.js
@@ -1,4 +1,4 @@
 export default async function handler(req, res) {
-  const { address } = req.body;
-  res.status(200).send('OK'); // Placeholder
+  const { address } = req.query;
+  res.json({ claimed: false }); // Placeholder
 }
